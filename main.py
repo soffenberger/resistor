7cbecdbefe3ee8c7d00ab7b6f40996bd497189a9
soffenberger <soffenbe@asu.edu>
2016-05-15 20:23:15 -0700

    Figured out save to database also a couple of ways to load from the database will work on a crawler to get image later on

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python
+from create_sql_engine import *
+import resistor_class as rc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///img.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+def load_from_database_id(id):
+	global session
+        return session.query(img).filter(img.id == id).all()
+
+def main():
+	im = load_from_database_id(int(1))
+	global session
+	image = session.query(img).first()
+	for i in im:
+		print i.name
+	print image.name
+
+
+if __name__ == '__main__':
+	print "Fuck Frank"
+	main()
